0c3a76e96c0cc5da71889361dddd8dcf89e8e77d
Jonas <jonas@gmail.com>
2023-03-08 12:50:27 +0100

    Version 4

--- final/tictactoe.js
+++ final/tictactoe.js
@@ -5,59 +5,28 @@
 /*
     - Comentar cabezara funciones
     - Poner las variables arriba e inicializarlas donde se necesite
-    - No errores en la consola
+    - No errores en la consola (dones)
     - Estilo que se vea bien
-    - Subirlo a github
-    - Metodos ganar 2 en 1
+    - Subirlo a github 
+    - Metodos ganar 2 en 1 (done)
 */
 
+// ZONA DECLARATIVA
+
+let contador_X = 0;
+let contador_O = 0;
+let turno = true;
 let jugador1 = 'X';
 let jugador2 = 'O';
-
 let x = [];
 let o = [];
-
-/**
- * [0, 1, 2]
- * [3, 4, 5]
- * [6, 7, 8]
- */
-// let tablero = ['','','','','','','','',''];
 let tablero = document.getElementsByClassName('casilla');
-
-// cada vez que se haga clic en la casilla me de el nombre de la posicion
-for(let i = 0; i < tablero.length; i++){
-    tablero[i].setAttribute('onclick', `pintaConsola(${i})`);
-}
-
-// Variable turno quiere decir que cuando su valor sea:
-//      - true = pintar x
-//      - false = pintar o
-
-let turno = true;
-
-function pintaConsola(numero){
-    if(turno){
-        tablero[numero].textContent = 'X';
-        GANAR_X();
-    } else {
-        tablero[numero].textContent = 'O';
-        GANAR_O();
-    }
-    tablero[numero].removeAttribute('onclick');
-    turno = !turno;
-}
-
-/**
- * [3, 4, 5]
- * [0, 3, 6]
- * [1, 4, 7]
- * [2, 5, 8]
- * [0, 4, 8]
- * [2, 4, 6]
- */
+let div_turno = document.getElementsByClassName('caja')[0];
+div_turno.textContent = 'Turno de X';
 let combinacionGanadora = [
+    [0, 1, 2],
     [3, 4, 5],
+    [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
@@ -65,43 +34,65 @@ let combinacionGanadora = [
     [2, 4, 6]
 ];
 
+// ZONA EJECUTIVA
+
+for(let i = 0; i < tablero.length; i++){
+    tablero[i].setAttribute('onclick', `pintaConsola(${i})`);
+}
 
 
 
-function GANAR_X(){
-    let actual = [];
-    // Recorrer las casillas para ver su contenido
+function pintaConsola(numero){
+    if(turno){
+        tablero[numero].textContent = 'X';
+        tablero[numero].removeAttribute('onclick');
+        ganador();
+        turno_O();
+        
+    } else {
+        tablero[numero].textContent = 'O';
+        tablero[numero].removeAttribute('onclick');
+        ganador();
+        turno_X();
+    }
+    turno = !turno
+}
+
+function ganador(){
     for (let i = 0; i < tablero.length; i++){
         if(tablero[i].innerHTML == 'X'){
-            actual.push(i);
+            x.push(i);
+        }
+        if(tablero[i].innerHTML == 'O'){
+            o.push(i);
         }
     }
-    /**
-     * Utilizar Array.include para comprobar si una de las combinaciones correctas
-     * esta incluida en mi array de actual
-     */
     for(let i = 0; i < combinacionGanadora.length ; i++){
-        if(actual.includes(combinacionGanadora[i][0]) && actual.includes(combinacionGanadora[i][1]) && actual.includes(combinacionGanadora[i][2])){
+        if(x.includes(combinacionGanadora[i][0]) && x.includes(combinacionGanadora[i][1]) && x.includes(combinacionGanadora[i][2])){
             alert('GANAN LAS X');
+            contador_X++;
+            reiniciar();
+        }
+        if(o.includes(combinacionGanadora[i][0]) && o.includes(combinacionGanadora[i][1]) && o.includes(combinacionGanadora[i][2])){
+            alert('GANAN LAS O');
+            contador_O++;
+            reiniciar();
         }
     }
 }
+function reiniciar(){
+    for(let i = 0; i < tablero.length; i++){
+        tablero[i].textContent = null;
+        tablero[i].setAttribute('onclick', `pintaConsola(${i})`);
+    }
+    x = [];
+    o = [];
+}
 
-function GANAR_O(){
-    let actual = [];
-    // Recorrer las casillas para ver su contenido
-    for (let i = 0; i < tablero.length; i++){
-        if(tablero[i].innerHTML == 'O'){
-            actual.push(i);
-        }
-    }
-    /**
-     * Utilizar Array.include para comprobar si una de las combinaciones correctas
-     * esta incluida en mi array de actual
-     */
-    for(let i = 0; i < combinacionGanadora.length ; i++){
-        if(actual.includes(combinacionGanadora[i][0]) && actual.includes(combinacionGanadora[i][1]) && actual.includes(combinacionGanadora[i][2])){
-            alert('GANAN LAS O');
-        }
-    }
+function turno_X(){
+    div_turno.textContent = 'Turno de X';
+}
+
+function turno_O(){
+    div_turno.textContent = 'Turno de O';
 }
